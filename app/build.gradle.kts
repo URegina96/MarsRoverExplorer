38f2f8b49609c9abfe7e9eacd51fc46b32305ea7
regina <usmanova.re@yandex.ru>
2024-10-25 19:02:18 +0500

    Build_gradle и libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("kotlin-kapt")
 }
 
 android {
@@ -8,8 +9,8 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.marsroverexplorer"
-        minSdk = 26
+        applicationId = "com.example.mooddiary"
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -29,19 +30,24 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         compose = true
     }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -49,21 +55,40 @@ android {
     }
 }
 
-dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+dependencies {
+    //реторофит
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.3")
+
+    implementation(libs.androidx.core.ktx)  // Основная библиотека Android KTX
+    implementation(libs.androidx.lifecycle.runtime.ktx)  // Жизненный цикл KTX
+    implementation(libs.androidx.activity.compose)  // Поддержка компонентов Activity для Compose
+    implementation(platform(libs.androidx.compose.bom))  // BOM для управления версиями Compose
+    implementation(libs.androidx.ui)  // Основной модуль Compose UI
+    implementation(libs.androidx.ui.graphics)  // Библиотека графики Compose
+    implementation(libs.androidx.ui.tooling.preview)  // Поддержка предпросмотра в Android Studio
+    implementation(libs.androidx.material3)  // Material Design 3 для Jetpack Compose
+    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    implementation(libs.androidx.room.runtime) // Основная библиотека Room
+    kapt(libs.androidx.room.compiler) // Компилятор Room для аннотаций
+    implementation(libs.androidx.room.ktx) // KTX версия Room для удобства использования
+    implementation ("androidx.core:core-ktx:1.13.1")
+
+
+    // Тестирование
+    testImplementation(libs.junit)  // JUnit для юнит-тестирования
+    androidTestImplementation(libs.androidx.junit)  // JUnit для UI тестирования
+    androidTestImplementation(libs.androidx.espresso.core)  // Espresso для UI тестов
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    androidTestImplementation(libs.androidx.ui.test.junit4)  // JUnit 4 для тестирования UI Compose
+
+    // Дебаг зависимости
+    debugImplementation(libs.androidx.ui.tooling)  // Предпросмотр инструментов
+    debugImplementation(libs.androidx.ui.test.manifest)  // Манифест для тестирования
+}
+kapt {
+    correctErrorTypes = true
 }
